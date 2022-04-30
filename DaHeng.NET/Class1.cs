a13472a324e4da879e710470ac8443669a0ccc08
ssccinng <whs11998@163.com>
2022-04-30 23:57:25 +0800

    add Daheng

---
+++ DaHeng.NET/Class1.cs
@@ -0,0 +1,15 @@
+﻿using DaHeng.NET;
+using GxIAPINET;
+using GxIAPINET;
+
+namespace DaHeng.NET
+{
+    public class Class1
+    {
+        public void Test()
+        {
+            IGXFactory.GetInstance().Init();
+
+        }
+    }
+}
---
+++ DaHeng.NET/DaHengCamera.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GxIAPINET;
+namespace DaHeng.NET
+{
+    public class DaHengCamera : IDisposable
+    {
+        public IGXFactory test => IGXFactory.GetInstance();
+        public DaHengCamera()
+        {
+            IGXFactory.GetInstance().Init();
+            
+        }
+        public void Dispose()
+        {
+            
+        }
+    }
+}
